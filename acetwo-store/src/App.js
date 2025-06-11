4b90909da733ffab383eb3d2be0ccbe7150816a7
Emmanuel Almonte <emmanuel.almonte@students.mq.edu.au>
2025-06-11 22:36:30 +1000

    Website Initial commit

---
+++ acetwo-store/src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello, World! Welcome to the Acetwo Store.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
