c2a045f6ef650c72d8164804694affb2bdfd982d
Emmanuel Almonte <emmanuel.almonte@students.mq.edu.au>
2025-06-13 21:46:53 +1000

    magnifier start. everything else complete

--- acetwo-store/src/pages/lockScreen.js
+++ acetwo-store/src/pages/lockScreen.js
@@ -22,17 +22,21 @@ const LockScreen = () => {
       <div className="lockscreen-content">
         <h1 className="lockscreen-title">Welcome to ACETWO</h1>
         <img src="/logo2.gif" alt="ACETWO Logo" className="lockscreen-logo" />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Enter store password"
-          className="lockscreen-input"
-        />
-        <button onClick={handleUnlock} className="lockscreen-button">
-          Enter
-        </button>
+        
+        <div className= "password-input-container">
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter store password"
+            className="lockscreen-input"
+          />
+          <button onClick={handleUnlock} className="lockscreen-button">
+            Enter
+          </button>
+        </div>
       </div>
+
       <div className="lockscreen-links">
         <Link to="/support" className="lockscreen-link">Customer Support</Link> |
         <Link to="/policies" className="lockscreen-link">Policies</Link>
