a7cdb913fa872a780a40d52a11ed38fbd7b88a63
Emmanuel Almonte <emmanuel.almonte@students.mq.edu.au>
2025-06-20 11:12:38 +1000

    redirection fix

--- acetwo-store/src/App.js
+++ acetwo-store/src/App.js
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 import LockScreen from "./pages/lockScreen";
 import Store from "./pages/home";
@@ -6,9 +8,24 @@ import FAQ from "./pages/support";
 import Policies from "./pages/policies";
 import ProtectedRoute from "./components/protectedRoute";
 
+function RedirectHandler() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const redirectTo = params.get("redirect");
+    if (redirectTo) {
+      navigate(redirectTo);
+    }
+  }, [navigate]);
+
+  return null;
+}
+
 function App() {
   return (
     <BrowserRouter basename="/">
+      <RedirectHandler />
       <Routes>
         <Route path="/" element={<LockScreen />} />
         <Route path="/support" element={<FAQ />} />
